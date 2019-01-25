420ed45e4e80577e713e7acb0eb8335064f0afa0
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-01-25 22:23:16 +0000

    general setup location, bar, globals

---
+++ plates/Assets/Scripts/Bar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour {
+
+    public string BarName;
+    public float Min;
+    public float Current;
+    public float Max;
+    public float DecayPerSecond;
+    public float Hotspot;
+    public Gameplay Gp;
+    public bool IsOnHotSpot;
+    public bool IsOverHotSpot;
+    public bool IsUnderHotSpot;
+    public float HotSpotToTop;
+    public float HotSpotToBottom;
+
+    // Use this for initialization
+    void Start () {
+        HotSpotToTop = Max - Hotspot;
+        HotSpotToBottom = Min + Hotspot;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Current -= Time.deltaTime * DecayPerSecond;
+        Current = Mathf.Min(Current, Max);
+        Current = Mathf.Max(Current, Min);
+
+        if (Current == Hotspot) {
+            IsOnHotSpot = true;
+        }
+        else {
+            IsOnHotSpot = false;
+        }
+
+        if (Current > Hotspot) {
+            IsOverHotSpot = true;
+        } else {
+            IsOverHotSpot = false;
+        }
+
+        if (Current < Hotspot) {
+            IsUnderHotSpot = true;
+        } else {
+            IsUnderHotSpot = false;
+        }
+
+        // TODO: CALCULATE HOW MUCH POINTS YOU GET DEPENDING ON HOW FAR AWAY YOUR ARE
+
+    }
+}
---
+++ plates/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour {
+
+    public float TimeCurrent;
+    public float TimeEnd;
+
+    public float EnergyMin;
+    public float EnergyCurrent;
+    public float EnergyMax;
+    public float EnergyRefillPerSecond;
+
+    public float ConnectednessCurrent;
+
+    public Dictionary<int, string> Locations = new Dictionary<int, string>();
+    public int LocationCurrentID;
+    public string LocationCurrentName;
+
+    public GameObject PrefabBar;
+
+    // Use this for initialization
+    void Start () {
+        Locations.Add(-1, "Munich");
+        Locations.Add(0, "Apartment");
+        Locations.Add(1, "Work");
+        LocationCurrentID = 0;
+        LocationCurrentName = Locations[LocationCurrentID];
+        GameObject barObject = Instantiate(PrefabBar);
+        Bar bar = barObject.GetComponent<Bar>();
+        bar.BarName = LocationCurrentName;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TimeCurrent += Time.deltaTime;
+        EnergyCurrent += Time.deltaTime * EnergyRefillPerSecond;
+        EnergyCurrent = Mathf.Min(EnergyCurrent, EnergyMax);
+        EnergyCurrent = Mathf.Max(EnergyCurrent, EnergyMin);
+        LocationCurrentName = Locations[LocationCurrentID];    
+    }
+}
