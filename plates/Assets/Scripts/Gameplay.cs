2267881bfd3106cdefaf0b86bec480ebe6001aa5
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-01-26 22:01:01 +0000

    make money money

--- plates/Assets/Scripts/Gameplay.cs
+++ plates/Assets/Scripts/Gameplay.cs
@@ -35,6 +35,8 @@ public class Gameplay : MonoBehaviour {
     public bool CameraIsMoving;
     public float CameraTransitionDuration;
 
+    public float MoneyGainPerSecond;
+
     // Use this for initialization
     void Start () {
         Locations.Add(0, "Munich");
@@ -90,6 +92,10 @@ public class Gameplay : MonoBehaviour {
 
         ConnectednessCurrent -= ConnectednessDecayPerSecond * Time.deltaTime;
         ConnectednessCurrent = Mathf.Max(ConnectednessCurrent, 0);
+
+        if (LocationCurrentID == 2) {
+            MoneyCurrent += MoneyGainPerSecond * Time.deltaTime;
+        };
     }
 
     public void IncreaseConnectedness(float v) {
