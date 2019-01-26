cf1c2c5a9a72f7c422f2e74414d290312247f538
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-01-26 14:51:18 +0000

    bar works!

--- plates/Assets/Scripts/Bar.cs
+++ plates/Assets/Scripts/Bar.cs
@@ -17,6 +17,12 @@ public class Bar : MonoBehaviour {
     public float ConnectednessPerSecondNormal;
     public float ConnectednessPerSecondHotspot;
     public float IncreasePerTap;
+    public GameObject ObjectCurrent;
+
+    // https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
+    float MapIntoRange (float s, float a1, float a2, float b1, float b2) {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
 
     // Use this for initialization
     void Start () {
@@ -44,6 +50,11 @@ public class Bar : MonoBehaviour {
                 gp.IncreaseConnectedness(ConnectednessPerSecondNormal * Time.deltaTime);
             }
         }
+
+        // y for 0 -> -2.36
+        // y for 1 -> 4.47
+        float currentY = MapIntoRange(Current, Min, Max, -2.36f, 4.47f);
+        ObjectCurrent.transform.localPosition = new Vector3(0.02f, currentY, -1.18f);
     }
 
     public void IncreaseCurrent() {
